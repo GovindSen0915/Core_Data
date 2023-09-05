fd228c37dae16b1b1ee524f0818d99dbaec6abb1
Govind Sen <govindsen0915@gmail.com>
2023-09-05 15:26:23 +0530

    chore(): add tableview cell with xib

---
+++ Core_Data/Screens/User/View/User.swift
@@ -0,0 +1,23 @@
+//
+//  User.swift
+//  Core_Data
+//
+//  Created by Govind Sen on 05/09/23.
+//
+
+import UIKit
+
+class User: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
